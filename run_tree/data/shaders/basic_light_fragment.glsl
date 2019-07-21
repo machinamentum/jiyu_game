2e38244dff6dc96b6a1f93f11b5b875c812596bb
Joshua Huelsman <joshuahuelsman@gmail.com>
2019-07-21 15:40:28 -0400

    Add basic lighting.

---
+++ run_tree/data/shaders/basic_light_fragment.glsl
@@ -0,0 +1,33 @@
+
+#version 330 core
+
+out vec4 fragment_color;
+
+in vec3 out_position;
+in vec3 out_normal;
+in vec4 out_color;
+
+struct Light {
+    vec3 position;
+};
+
+const int MAX_LIGHTS = 8;
+uniform Light lights[MAX_LIGHTS];
+uniform int num_lights;
+
+void main() {
+    
+    vec4 accum = vec4(0);
+    for (int i = 0; i < 1; ++i) {
+        vec4 light_color = vec4(1, 1, 1, 1);
+        vec3 position = lights[i].position;
+        
+        vec3 P = normalize(position - out_position);
+        vec3 N = normalize(out_normal);
+        
+        accum += dot(N, P) * light_color * out_color;
+    }
+    fragment_color = accum;
+}
+
+
---
+++ run_tree/data/shaders/basic_light_vertex.glsl
@@ -0,0 +1,23 @@
+
+#version 330 core
+
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec3 in_normal;
+layout (location = 2) in vec2 in_tex_coord;
+
+out vec4 out_color;
+out vec3 out_position;
+out vec3 out_normal;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+void main() {
+    out_color = vec4(1, 0, 0, 1);
+    out_position = (projection * view * model * vec4(in_position, 1)).xyz;
+    out_normal = in_normal;
+    gl_Position = projection * view * model * vec4(in_position, 1);
+}
+
+
